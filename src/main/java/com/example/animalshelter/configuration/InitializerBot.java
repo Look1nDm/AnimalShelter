9fa25674b9f0f8865e5c610596bc3966e1dd3974
dimka <dimka411841994@gmail.com>
2022-12-07 11:50:10 +0300

    Добавлен ответ на стартовую команду пользователю

---
+++ src/main/java/com/example/animalshelter/configuration/InitializerBot.java
@@ -0,0 +1,26 @@
+package com.example.animalshelter.configuration;
+
+import com.example.animalshelter.service.TelegramBot;
+import lombok.AllArgsConstructor;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+@AllArgsConstructor
+public class InitializerBot {
+    private TelegramBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException{
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e){
+
+        }
+    }
+}
--- src/main/java/com/example/animalshelter/service/TelegramBot.java
+++ src/main/java/com/example/animalshelter/service/TelegramBot.java
@@ -4,7 +4,9 @@ import com.example.animalshelter.configuration.ConfigurationBot;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 @AllArgsConstructor
@@ -25,6 +27,40 @@ public class TelegramBot extends TelegramLongPollingBot {
      */
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage()&&update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            String userName = update.getMessage().getChat().getFirstName();
+            switch (messageText){
+                case "/start":
+                        startCommand(chatId,userName);
+                        break;
+                default:
+                        sendMessage(chatId,"Пока что данная функция не поддреживается");
+            }
+        }
+    }
 
+    /**
+     * Приветственное сообщение от бота
+     * @param chatId
+     * @param userName
+     */
+    private void startCommand (Long chatId,String userName) {
+
+        String answer = "Приветствую "+userName+" . Я бот-помощник приюта для собак." +
+                " Чтобы Вы хотели узнать?";
+        sendMessage(chatId, answer);
+    }
+    private void sendMessage(Long chatId, String textToSend){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e){
+
+        }
     }
 }
